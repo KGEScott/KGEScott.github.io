f460b03a4e1fb5b41295bc0e81d1839d808ffa38
KGEScott <70236679+KGEScott@users.noreply.github.com>
2023-03-15 04:58:50 -0400

    Changed Portfolio
    Modified portfolio to have projects viewed. Added photos and made it so when expanding a photo, you can switch between more.

---
+++ script.js
@@ -0,0 +1,76 @@
+const projectsImages = [
+    [
+        "images/login.png",
+        "images/searchpublishers.png",
+        "images/viewserieswithinpub.png",
+        "images/viewcomicwithinseries.png",
+        "images/mycollection.png",
+        "images/viewcoverart.png",
+        "images/exportcollection.png"
+    ],
+    [
+        "images/elginvapeshop.png",
+        "images/bestselling.png",
+        "images/disposable.png",
+        "images/disposable2.png",
+        "images/adminpage.png",
+        "images/addproduct.png",
+        "images/delproduct.png",
+        "images/updateproduct.png"
+    ],
+    // Add more projects
+];
+
+function openModal(projectIndex, imageIndex) {
+    const modal = document.getElementById("image-modal");
+    const modalImage = document.getElementById("modal-image");
+    modal.style.display = "block";
+    modalImage.src = projectsImages[projectIndex][imageIndex];
+    modalImage.dataset.projectIndex = projectIndex;
+    modalImage.dataset.imageIndex = imageIndex;
+}
+
+function closeModal() {
+    const modal = document.getElementById("image-modal");
+    modal.style.display = "none";
+}
+
+function changeImage(direction) {
+    const modalImage = document.getElementById("modal-image");
+    const projectIndex = parseInt(modalImage.dataset.projectIndex);
+    const currentImageIndex = parseInt(modalImage.dataset.imageIndex);
+    const images = projectsImages[projectIndex];
+    let newIndex = currentImageIndex + direction;
+
+    if (newIndex < 0) {
+        newIndex = images.length - 1;
+    } else if (newIndex >= images.length) {
+        newIndex = 0;
+    }
+
+    modalImage.src = images[newIndex];
+    modalImage.dataset.imageIndex = newIndex;
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    document.querySelectorAll('.project').forEach((project, projectIndex) => {
+        project.querySelector("img").addEventListener("click", () => {
+            openModal(projectIndex, 0);
+        });
+    });
+
+    document.getElementById("image-modal").addEventListener("click", (event) => {
+        if (event.target.tagName !== "IMG") {
+            closeModal();
+        }
+    });
+
+    document.getElementById("prev-button").addEventListener("click", (event) => {
+        event.stopPropagation();
+        changeImage(-1);
+    });
+    document.getElementById("next-button").addEventListener("click", (event) => {
+        event.stopPropagation();
+        changeImage(1);
+    });
+});
